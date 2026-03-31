c7329a9e2a4f6f9b32438ce169d57963673bc161
Link4Electronics <link4electronics@gmail.com>
2026-03-31 11:41:25 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -8,7 +8,6 @@ export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export DESKTOP=/usr/share/applications/hp15c.desktop
 export ALWAYS_SOFTWARE=1
 
 # Deploy dependencies
