da7a5f021b7c8f3ad40b444a8f169e7060804f6f
Link4Electronics <link4electronics@gmail.com>
2026-03-31 11:11:38 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,21 +6,20 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm tcl tk
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
-# Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
+echo "Getting app..."
+echo "---------------------------------------------------------------"
+VERSION=5.1.00
+echo "$VERSION" > ~/version
+wget https://hp-15c-simulator.de/versions/${VERSION}/HP-15C_${VERSION}_Source.zip
+bsdtar -xf ./HP-15C_${VERSION}_Source.zip
 
-# If the application needs to be manually built that has to be done down here
-
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
-#
-# if [ "${DEVEL_RELEASE-}" = 1 ]; then
-# 	nightly build steps
-# else
-# 	regular build steps
-# fi
+mkdir -p ./AppDir/bin
+mkdir -p ./AppDir/share/fonts
+mv -v css lib msgs images HP-15C.tcl ./AppDir/bin
+mv -v HP-15C_Simulator_Font.ttf ./AppDir/share/fonts
