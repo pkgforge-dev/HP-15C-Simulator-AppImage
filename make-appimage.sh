987f1f978dbbd2ac25e6ec99b4b0b6085b4f0813
Link4Electronics <link4electronics@gmail.com>
2026-03-31 12:45:42 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -13,7 +13,7 @@ export STARTUPWMCLASS=Hp-15c.tcl
 export ALWAYS_SOFTWARE=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/wish ./AppDir/bin/*
+quick-sharun /usr/bin/wish /usr/lib/tcl8.6/init.tcl ./AppDir/bin/*
 
 # Additional changes can be done in between here
 mkdir -p ./AppDir/share/fonts
