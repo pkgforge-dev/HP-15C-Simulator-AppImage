f670b6350e2ff0eaae3ad44361ee07cecd7294ef
Link4Electronics <link4electronics@gmail.com>
2026-08-29 15:52:59 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,7 +10,7 @@ export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export DESKTOP=/usr/share/applications/hp15c.desktop
 export STARTUPWMCLASS=Hp-15c.tcl
-export ALWAYS_SOFTWARE=1
+export USE_HOST_DRIVERS_EXPERIMENTAL=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/wish ./AppDir/bin/*
