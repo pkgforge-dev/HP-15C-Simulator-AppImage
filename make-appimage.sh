7cb8ca7215ce28b0516fbe75d1e733cd176b856b
Link4Electronics <link4electronics@gmail.com>
2026-08-29 18:21:03 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,8 +3,7 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q hp15c | awk '{print $2; exit}')
-export ARCH VERSION
+export ARCH
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
@@ -15,14 +14,5 @@ export USE_HOST_DRIVERS_EXPERIMENTAL=1
 # Deploy dependencies
 quick-sharun /usr/bin/wish ./AppDir/bin/*
 
-mkdir -p ./AppDir/share/fonts
-cp -v /usr/share/fonts/HP-15C_Simulator_Font.ttf ./AppDir/share/fonts
-cp -r /usr/lib/tcl8* ./AppDir/shared/lib
-cp -r /usr/lib/tk8.6 ./AppDir/shared/lib
-
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
-
-# Test the app for 12 seconds, if the test fails due to the app
-# having issues running in the CI use --simple-test instead
-quick-sharun --test ./dist/*.AppImage
