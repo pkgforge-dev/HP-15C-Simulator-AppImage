19b34783c95f5f91e1ed92f2135e1ac0af17d29b
Link4Electronics <link4electronics@gmail.com>
2026-08-29 18:21:25 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,15 +6,20 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm tcl tk
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-make-aur-package hp15c
+#make-aur-package hp15c
 
 # If the application needs to be manually built that has to be done down here
 mkdir -p ./AppDir/bin
 mv -v /usr/lib/hp15c/* ./AppDir/bin
+
+mkdir -p ./AppDir/share/fonts
+cp -v /usr/share/fonts/HP-15C_Simulator_Font.ttf ./AppDir/share/fonts
+cp -r /usr/lib/tcl8* ./AppDir/shared/lib
+cp -r /usr/lib/tk8.6 ./AppDir/shared/lib
