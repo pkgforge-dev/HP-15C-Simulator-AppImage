a11752d591a00802d32cbcf172e2a55c6222a292
Link4Electronics <link4electronics@gmail.com>
2026-08-30 07:07:36 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -7,7 +7,7 @@ export ARCH
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export USE_HOST_DRIVERS_EXPERIMENTAL=1
+export ALWAYS_SOFTWARE=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/wish /usr/lib/tcl8* /usr/lib/tk8.6 ./AppDir/bin/*
